e19d0edf7826436d6bce84a90400f6b926c34497
Jerry-Khobby <jerrymardeburg@gmail.com>
2023-12-04 05:04:12 -1200

    done with learning how to send emails

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,19 @@
 import requests
-import xlwt 
-from xlwt import Workbook 
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.application import MIMEApplication
+from email import encoders
+import smtplib
+from xlwt import Workbook
+from email.utils import COMMASPACE,formatdate
+from os.path import basename
+
+
+
+
+
+#https://myaccount.google.com/lesssecureapps
 
 API_URL = "https://remoteok.com/api"
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
@@ -29,6 +42,33 @@ def output_jobs_to_xls(data):
 
     wb.save("Remote_jobs.xls")
 
+def send_email(send_from, send_to, subject, text, files=None):
+    assert isinstance(send_to, list)
+    msg = MIMEMultipart()
+    msg["From"] = send_from
+    msg["To"] = COMMASPACE.join(send_to)
+    msg["Date"] = formatdate(localtime=True)
+    msg["Subject"] = subject
+    msg.attach(MIMEText(text))
+
+    for f in files or []:
+        with open(f, "rb") as fil:
+            part = MIMEApplication(fil.read(),Name=basename(f))
+            part["Content-Disposition"]= f'attachment; filename="{basename(f)}"'
+        msg.attach(part)
+    smtp=smtplib.SMTP('stmp.gmail.com:587')
+    smtp.starttls()
+    smtp.login(send_from,'priscillatola')
+    smtp.sendmail(send_from,send_to,msg.as_string())
+    smtp.close()
+
+        
+
+
+
+
+
 if __name__ == "__main__":
     json_data = get_remote_ok_jobs()[1:]
     output_jobs_to_xls(json_data)
+    send_email("jerrymardeburg@gmail.com",["jerrymardeburg@gmail.com"],"Jobs Posting","Please find the attached a list of job posting to this  email",files=["Remote_jobs.xls"])
